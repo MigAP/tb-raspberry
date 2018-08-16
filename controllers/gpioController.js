ccb0bd22f202bb27638dccce88492a5d0eaea00e
MigAP <miguel.arpa.p@gmail.com>
2018-08-16 11:48:02 +0200

    add package dependencies and fix gpioController response

--- controllers/gpioController.js
+++ controllers/gpioController.js
@@ -7,10 +7,10 @@ exports.gpio = function (req, res){
 exports.gpioWrite = function (req, res){
 	const port = new Gpio(req.params.port, {mode:Gpio.OUTPUT});
 	port.digitalWrite(req.params.state);
-	res.end('The state of th GPIO ' + req.params.port +' is : '+req.params.state);
+	res.end('The state of the GPIO ' + req.params.port +' is : '+req.params.state);
 };
 
 exports.gpioRead = function (req, res) {
 	const port = new Gpio(req.params.port);
-	res.end('The state of th GPIO ' + req.params.port + ' is : ' +  port.digitalRead());
+	res.end('The state of the GPIO ' + req.params.port + ' is : ' +  port.digitalRead());
 }
